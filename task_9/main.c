331217fa27cb456dcf8db5626ea05ed4e4ff050d
Fedor Gerasimov <f.gerasimov@g.nsu.ru>
2020-12-11 08:03:35 +0700

    Initial Commit

---
+++ task_9/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main()
+{
+	int pid;
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Fork failure");
+		return 1;
+	}
+
+	//child
+	if (pid == 0)
+	{
+		if(execlp("cat", "cat", "text.txt", (char*) 0) == -1)
+		{
+			perror("Execute cat failure");
+			return 1;
+		}
+	} else
+	//parent
+	{
+		int childStat;
+		printf("Parent begin\n");
+
+		errno = 0;
+		while(wait(&childStat) == -1)
+		{
+			if(errno == EINTR)
+			{
+				errno = 0;
+				continue;
+			}
+		}
+
+		printf("Parent end\n");
+	}
+	return 0;
+}
