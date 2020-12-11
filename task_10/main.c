bdf0882c8aeafedd5af380e7fa1475c1770a9307
Fedor Gerasimov <f.gerasimov@g.nsu.ru>
2020-12-11 08:26:13 +0700

    Initial Commit

---
+++ task_10/main.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <errno.h>
+
+int main(int argc, char* argv[])
+{
+	if(argc == 1)
+	{
+		printf("usage: ./prog [cmd] [arg]\n");
+		return 1;
+	}
+
+	int pid;
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Fork failure");
+		return 1;
+	}
+
+	//child
+	if(pid == 0)
+	{
+		if(execvp(argv[1], argv + 1) == -1)
+		{
+			perror("Execute argv error");
+			return 1;
+		}
+	} else
+	//parent
+	{
+		int childStat;
+		while(wait(&childStat) == -1)
+		{
+			if (errno == EINTR)
+			{
+				errno = 0;
+				continue;
+			}
+			perror("Wait failure");
+			return 1;
+		}
+		printf("parent message\n");
+	}
+
+	return 0;
+}
