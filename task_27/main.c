c00ebf05c76760b0175a4ced7759a6ad6099340e
Fedor Gerasimov <75016999+akademwalker@users.noreply.github.com>
2020-12-24 22:02:33 +0700

    Update main.c
    Error check added

--- task_27/main.c
+++ task_27/main.c
@@ -3,7 +3,27 @@
 #include <errno.h>
 #include <ctype.h>
 
+#include <wait.h>
+
 #define MAXBUFF 64
+
+void printStat(int stat)
+{
+	if(WCOREDUMP(stat))
+		printf("Process ended with core dump %d\n", WCOREDUMP(stat));
+	if(WEXITSTATUS(stat))
+		printf("Process ended with exit code %d\n", WEXITSTATUS(stat));
+	if(WIFCONTINUED(stat))
+		printf("Process continued %d", WIFCONTINUED(stat));
+	if(WIFSIGNALED(stat))
+		printf("Process terminated due to signal", WIFSIGNALED(stat));
+	if(WIFSTOPPED(stat))
+		printf("Process currently stopped\n");
+	if(WSTOPSIG(stat))
+		printf("Process stopped due to signal %d", WSTOPSIG(stat));
+	if(WTERMSIG(stat))
+		printf("Process terminated due to signal %d", WTERMSIG(stat));
+}
 
 int main()
 {
@@ -34,9 +54,16 @@ int main()
 		}
 		break;
 	}
-	if(pclose(in) == -1)
+	
+	
+	int stat = pclose(in);
+	if(stat == -1)
 	{
 		perror("Error in closing pipe");
+		return -1;
+	} else if(!WIFEXITED(stat))
+	{
+		printStat(stat);
 		return 1;
 	}
 
