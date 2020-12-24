487fb065b3f789914f990c867f137a6924b6116b
Fedor Gerasimov <f.gerasimov@g.nsu.ru>
2020-12-24 21:57:09 +0700

    Error check added

--- task_26/main.c
+++ task_26/main.c
@@ -2,13 +2,32 @@
 #include <stdio.h>
 #include <errno.h>
 #include <ctype.h>
-
+#include <wait.h>
 
 #define MAXBUFF 64
+
+void printStat(int stat)
+{
+	if(WCOREDUMP(stat))
+		printf("Process ended with core dump %d\n", WCOREDUMP(stat));
+	if(WEXITSTATUS(stat))
+		printf("Process ended with exit code %d\n", WEXITSTATUS(stat));
+	if(WIFCONTINUED(stat))
+		printf("Process continued %d", WIFCONTINUED(stat));
+	if(WIFSIGNALED(stat))
+		printf("Process terminated due to signal", WIFSIGNALED(stat));
+	if(WIFSTOPPED(stat))
+		printf("Process currently stopped\n");
+	if(WSTOPSIG(stat))
+		printf("Process stopped due to signal %d", WSTOPSIG(stat));
+	if(WTERMSIG(stat))
+		printf("Process terminated due to signal %d", WTERMSIG(stat));
+}
 
 void makeUpper(char* buff, size_t n)
 {
 	for(size_t i = 0; i < n; ++i)
+
 		buff[i] = toupper(buff[i]);
 }
 
@@ -20,6 +39,7 @@ int main()
 		perror("Error in opening pipe");
 		return 1;
 	}
+
 
 	size_t count;
 	char buff[MAXBUFF];
@@ -40,9 +60,15 @@ int main()
 		}
 		break;
 	}
-	if(pclose(in) == -1)
+
+	int stat = pclose(in);
+	if(stat == -1)
 	{
 		perror("Error in closing pipe");
+		return -1;
+	} else if(!WIFEXITED(stat))
+	{
+		printStat(stat);
 		return 1;
 	}
 
